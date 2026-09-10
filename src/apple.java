a0115cafa4d339a1fa977b13e297694d4e20ffcc
xuehao <1072819749@qq.com>
2026-09-10 22:36:17 +0800

    单1

--- src/apple.java
+++ src/apple.java
@@ -1,6 +1,6 @@
 public class apple {
     public static  void main(String[] args) {
         System.out.println("苹果111");
-        System.out.println("苹果222");
+        System.out.println("1");
     }
 }
